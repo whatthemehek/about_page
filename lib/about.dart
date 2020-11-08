da40408dd23a03f3f234f5ea101a5202ae057462
mook <mehek@live.com>
2020-11-08 12:43:03 -0800

    added about page

--- lib/about.dart
+++ lib/about.dart
@@ -16,6 +16,7 @@ class AboutPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    double c_width = MediaQuery.of(context).size.width*0.5;
     return Scaffold(
       appBar: MyAppBar(),
       body: ListView (
@@ -45,11 +46,28 @@ class AboutPage extends StatelessWidget{
               ),
             ),
           ),
-//          Container(
-//            height: 500,
-//            color: Colors.amber[400],
-//            child: const Center(child: Text('Entry C')),
-//          ),
+          Container(
+            height: 500,
+            color: Colors.amber[400],
+            child: Padding (
+              padding: const EdgeInsets.all(16.0),
+              child: Center (
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image(image: AssetImage('graphics/Headshot.jpg')),
+                    Container (
+                      width: c_width,
+                      child: Text (
+                          "I am a high school senior at the Overlake School, interested in technology, product design, and software development! I have a passion for universal design, especially in the field of music education, which led me to develop my innovative tool, the Mehek Box. I'm also active in social justice work at Overlake, leading the IMPACT Club to write anti-racism webinars, push for curriculum reform, and bring a focus on cultural competency into faculty hiring. I hope to continue working on socially-conscious, design-forward projects, and keep learning! ",
+                          textAlign: TextAlign.left
+                      ),
+                    )
+                  ]
+                )
+              )
+            )
+          ),
 //          Container(
 //            height: 500,
 //            color: Colors.amber[300],
